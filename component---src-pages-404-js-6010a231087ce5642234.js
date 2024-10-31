1b9cb46c5f3a1ea59ba937b58859782191ca4331
Sandor Lorand <sandor.lorand@HXDE0148.local>
2024-10-31 16:55:37 +0100

    Updates

--- component---src-pages-404-js-6010a231087ce5642234.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunknew_salspot_software_landing=self.webpackChunknew_salspot_software_landing||[]).push([[883],{429:function(e,n,t){t.r(n),t.d(n,{default:function(){return u}});var a=t(7294);function u(){return a.createElement("div",null," 404 Page Not found Redirecting to home")}}}]);
-//# sourceMappingURL=component---src-pages-404-js-6010a231087ce5642234.js.map
---
+++ component---src-pages-404-js-91c5e2a99f4a3b17a820.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunknew_salspot_software_landing=self.webpackChunknew_salspot_software_landing||[]).push([[125],{3331:function(e,n,t){t.r(n),t.d(n,{default:function(){return u}});var a=t(6540);function u(){return a.createElement("div",null," 404 Page Not found Redirecting to home")}}}]);
+//# sourceMappingURL=component---src-pages-404-js-91c5e2a99f4a3b17a820.js.map
